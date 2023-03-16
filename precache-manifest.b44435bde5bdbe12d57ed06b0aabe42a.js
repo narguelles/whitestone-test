40a1560321775070a78131e8f4c649ceaf4723fe
norielarguelles <noriel.arguelles@kmc.solutions>
2023-03-17 01:32:10 +0800

    iinit

---
+++ precache-manifest.b44435bde5bdbe12d57ed06b0aabe42a.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38a2b58df330c85e0029eecf71d7c26",
+    "url": "_redirects"
+  },
+  {
+    "revision": "9a26d563f5ee23983d6e",
+    "url": "css/admin.ed178950.css"
+  },
+  {
+    "revision": "4f9a5e2a0cb69e025ecb",
+    "url": "css/app.da7cf09c.css"
+  },
+  {
+    "revision": "62d5afca7ac86eea9781",
+    "url": "css/client.ed178950.css"
+  },
+  {
+    "revision": "4fb3bd39fc684bab210b",
+    "url": "css/funddetails.e21d3088.css"
+  },
+  {
+    "revision": "70194040c4fa5f5f8e0a",
+    "url": "css/funddetails~fundoverview~propertiesadd~propertiesdetails~propertiesedit~property.ce091044.css"
+  },
+  {
+    "revision": "621dcf9039d87f57d4d1",
+    "url": "css/fundoverview.1c448188.css"
+  },
+  {
+    "revision": "85d211a0c6175d1810f3",
+    "url": "css/investordetails.697aa424.css"
+  },
+  {
+    "revision": "a5c9be213e9aa16b2be1",
+    "url": "css/investorlist.cf84fabd.css"
+  },
+  {
+    "revision": "415d2e863212af67da5e",
+    "url": "css/login.ed178950.css"
+  },
+  {
+    "revision": "462f0fb5a35b06a932d4",
+    "url": "css/propertiesdetails.e4f82d64.css"
+  },
+  {
+    "revision": "81d8b2475f4b2d4d2ffe",
+    "url": "css/propertiesdetails~property.3ed72647.css"
+  },
+  {
+    "revision": "abe71f7d608d43b56d9b2aef78d7ae99",
+    "url": "fonts/element-icons.abe71f7d.ttf"
+  },
+  {
+    "revision": "d9491be2c5109fca0fa40d0c59e2e3b9",
+    "url": "fonts/element-icons.d9491be2.woff"
+  },
+  {
+    "revision": "bea7e0b7986f20389823e8e108208c3a",
+    "url": "fonts/icofont.css"
+  },
+  {
+    "revision": "c20821f3c118543467985d0c01114cae",
+    "url": "fonts/icofont.min.css"
+  },
+  {
+    "revision": "13e892a28dda2567a981344188df5567",
+    "url": "fonts/lg.13e892a2.ttf"
+  },
+  {
+    "revision": "ba38ec746a64d70d0a68e838664d3418",
+    "url": "fonts/lg.ba38ec74.woff"
+  },
+  {
+    "revision": "db9f4533754ce28129fbf9507c9b1240",
+    "url": "img/lg.db9f4533.svg"
+  },
+  {
+    "revision": "bbdac9cda255c54bfd809110aff87898",
+    "url": "img/loading.bbdac9cd.gif"
+  },
+  {
+    "revision": "a1f6fdef96f8312ec37b1ffa2128aa9d",
+    "url": "index.html"
+  },
+  {
+    "revision": "9a26d563f5ee23983d6e",
+    "url": "js/admin.3e8ae88e.js"
+  },
+  {
+    "revision": "4f9a5e2a0cb69e025ecb",
+    "url": "js/app.e00470fd.js"
+  },
+  {
+    "revision": "40fc04023a2061360139",
+    "url": "js/changepassword.48aa275f.js"
+  },
+  {
+    "revision": "aa4dbd01dc80e9e13591",
+    "url": "js/chunk-vendors.384c9478.js"
+  },
+  {
+    "revision": "62d5afca7ac86eea9781",
+    "url": "js/client.ae44de12.js"
+  },
+  {
+    "revision": "7f29b180b9717e29a6a1",
+    "url": "js/client~funddetails~fundoverview~investordetails~investorlist~propertiesdetails~propertieslist~upcomingfunds.c2d3d1bc.js"
+  },
+  {
+    "revision": "20f0efd3d05a602a2c75",
+    "url": "js/client~funddetails~fundoverview~investorlist~preferences~propertiesadd~propertiesedit~propertieslist.ea5fc997.js"
+  },
+  {
+    "revision": "73aa08de17b2668038d8",
+    "url": "js/client~preferences~propertiesadd~propertiesedit~upcomingfunds.c0359888.js"
+  },
+  {
+    "revision": "31b24556168fe0d08916",
+    "url": "js/dashboard.063a050c.js"
+  },
+  {
+    "revision": "bceac8009c61e111b6d4",
+    "url": "js/documents.4a1d1be4.js"
+  },
+  {
+    "revision": "94d0ab1fb5321528fcb2",
+    "url": "js/documents~funddetails~fundoverview~investordetails~investorlist~preferences~propertiesadd~properties~aebdb884.9796f7bd.js"
+  },
+  {
+    "revision": "a30ab4ae17515580582a",
+    "url": "js/documents~investordetails~preferences.bf95cf82.js"
+  },
+  {
+    "revision": "4fb3bd39fc684bab210b",
+    "url": "js/funddetails.05195326.js"
+  },
+  {
+    "revision": "70194040c4fa5f5f8e0a",
+    "url": "js/funddetails~fundoverview~propertiesadd~propertiesdetails~propertiesedit~property.d26619dd.js"
+  },
+  {
+    "revision": "621dcf9039d87f57d4d1",
+    "url": "js/fundoverview.fea7a365.js"
+  },
+  {
+    "revision": "e28c6920082bbf8fbe0e",
+    "url": "js/investoradd.37d6aeaf.js"
+  },
+  {
+    "revision": "cfbf2e24b3e604a999d9",
+    "url": "js/investoradd~investoredit~investorlist.308bebea.js"
+  },
+  {
+    "revision": "85d211a0c6175d1810f3",
+    "url": "js/investordetails.f50c60b8.js"
+  },
+  {
+    "revision": "866e816a511e647acbd4",
+    "url": "js/investoredit.43697230.js"
+  },
+  {
+    "revision": "a5c9be213e9aa16b2be1",
+    "url": "js/investorlist.e7baef0e.js"
+  },
+  {
+    "revision": "7eeaab76f25a47ddc066",
+    "url": "js/investors.10e0de80.js"
+  },
+  {
+    "revision": "415d2e863212af67da5e",
+    "url": "js/login.dff0a967.js"
+  },
+  {
+    "revision": "0b0684f38075108ed559",
+    "url": "js/preferences.5e741647.js"
+  },
+  {
+    "revision": "8146f1383a32b28afdea",
+    "url": "js/properties.e6bb4f21.js"
+  },
+  {
+    "revision": "95ff1417a33f619dfcce",
+    "url": "js/propertiesadd.f0b4cfc7.js"
+  },
+  {
+    "revision": "462f0fb5a35b06a932d4",
+    "url": "js/propertiesdetails.fe4b2cc8.js"
+  },
+  {
+    "revision": "81d8b2475f4b2d4d2ffe",
+    "url": "js/propertiesdetails~property.08df86af.js"
+  },
+  {
+    "revision": "0459d1d94ffdfaee3eb2",
+    "url": "js/propertiesedit.7f892019.js"
+  },
+  {
+    "revision": "b948b59769b6bde8dca3",
+    "url": "js/propertieslist.e4160f02.js"
+  },
+  {
+    "revision": "8a960b0683949d43beb7",
+    "url": "js/property.d536bbe1.js"
+  },
+  {
+    "revision": "bac43c79974127521044",
+    "url": "js/upcomingfunds.83b96b2b.js"
+  },
+  {
+    "revision": "cfddced3821f7707f1a25182b2f59762",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "bffabd0b73e99af337dd8bbedfa916ba",
+    "url": "routes.json"
+  },
+  {
+    "revision": "b0a7414da494ff58198b6ec5d1b83d78",
+    "url": "staticwebapp.config.json"
+  }
+]);
